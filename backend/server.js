1ab151f0c58e260cf9b7f63002a8c2e7185cc4a5
Chanti-Lingamaiah777 <chantilingamaiah@gmail.com>
2025-10-08 16:43:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://yourdomain.com'] 
+    ? ['https://fullstack-food-app-lovat.vercel.app/'] 
     : ['http://localhost:3000'],
   credentials: true
 }));
